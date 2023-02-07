45f83bebaf5d1060449d98f2c08a8a961975c036
hadidamir87 <hadi66damirhci87@gmail.com>
2023-02-07 15:18:20 +0330

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-    Node01 node01= new Node01();
-    node01.appendToTail(10);
-//    Node01 node02= new Node01();
-//    Node01 node011= new Node01(node01,50);
-//    Node01 node012= new Node01(node011,10);
-//    node01.appendToTail(50);
-//    node011.appendToTail(10);
-        System.out.println(node01);
-        System.out.println(node01);
-        System.out.println(node01);
-        System.out.println(node01);
-
+        Node01 node = new Node01(10);
+        Node01 node2 = new Node01(2);
+        Node01 node01= new Node01();
+        node.appendToTail(1);
+        node.appendToTail(2);
+        node.appendToTail(3);
+        node.appendToTail(4);
+        node.appendToTail(5);
+        node2.appendToTail(12);
+     //   System.out.println(node);
+//        System.out.println(node2);
+        System.out.println(node01.remove(node01,null));
+        //   System.out.println(node.remove(node, 3));
+        //   System.out.println(node);
     }
 }
--- src/Node01.java
+++ src/Node01.java
@@ -1,26 +1,46 @@
 public class Node01 {
     private Node01 next;
-    private int data;
+    private Integer data;
 
     public Node01() {
     }
 
-    public Node01(Node01 next, int data) {
-        this.next = next;
+    public Node01(int data) {
+        //     this.next = null;
         this.data = data;
     }
+    public void appendToTail(int d) {
 
-    public Node01 getNext() {
-        return next;
+        Node01 temp = this;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node01 node01New = new Node01(d);
+        temp.next = node01New;
     }
 
-    public void setNext(Node01 next) {
-        this.next = next;
+    public void print(){
+Node01 temp = this;
+        System.out.println(this.next);
+        System.out.println(temp.next.next);
+        System.out.println(this);
     }
-    public void appendToTail(int d){
-     Node01 node = new Node01(next , d);
-     next = node;
-    }
+    public Node01 remove(Node01 headNode, Integer d) {
+        Node01 temp = this;
+        if (temp.data==null)
+
+      if(temp.data==d){
+          return null;
+      }
+      while (temp.next!=null) {
+          if (temp.next.data == d) {
+              temp.next = temp.next.next;
+              break;
+          }
+          temp = temp.next;
+      }
+        return headNode;
+}
 
     @Override
     public String toString() {
@@ -29,4 +49,13 @@ public class Node01 {
                 ", data=" + data +
                 '}';
     }
+//    @Override
+//    public String toString() {
+//        String res="";
+//        Node01 temp = this;
+//        while (temp!=null){
+//            res = res + " " + temp.data +"\n";
+//            temp= temp.next;
+//        }
+//        return res;}
 }
